7688c12cb5c8d95122fe7fc9d7bd89051e427c50
phamcongthanhvn2k6 <phamcongt56@gmail.com>
2025-11-14 10:48:15 +0700

    fix-deloy

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import RouterSetUp from './RouterSetUp'
 
 function App() {
--- src/RouterSetUp.tsx
+++ src/RouterSetUp.tsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Start from './page/StartPage/Start'
 import OpeningLetter from './page/OpeningLetter/OpeningLetter'
